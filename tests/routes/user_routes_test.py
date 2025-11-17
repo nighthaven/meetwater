96cf915a94bc52767a848fce0e6eaccb6d8738bc
Boris Le Bon <ooborislebonoo@gmail.com>
2025-11-18 00:08:50 +0100

    [tests]: add tests for routes users

--- tests/routes/user_routes_test.py
+++ tests/routes/user_routes_test.py
@@ -1,5 +1,9 @@
 from datetime import date
 
+from src.models.enums.swimmer_level import SwimmerLevel
+from tests.fixtures.swimmer_factory import SwimmerFactory
+from tests.fixtures.user_factory import UserFactory
+
 
 class TestCreateUser:
     def test_create_users(self, client):
@@ -14,3 +18,36 @@ class TestCreateUser:
             },
         )
         assert response.status_code == 201
+
+
+class TestCreateSwimmer:
+    def test_create_swimmers(self, client):
+        user = UserFactory()
+        payload = {
+            "first_name": "frodo",
+            "last_name": "Saquet",
+            "birth_date": date(2000, 1, 16).isoformat(),
+            "level": SwimmerLevel.INTERMEDIATE.value,
+        }
+        response = client.post(
+            f"/users/{user.id}/swimmer",
+            json=payload,
+        )
+        assert response.status_code == 201
+
+
+class TestGetUser:
+    def test_get_user_by_id(self, client):
+        user = UserFactory()
+        swimmer = SwimmerFactory(link_user=user)
+        response = client.get(
+            "/users/{}".format(user.id),
+        )
+        assert response.status_code == 200
+        assert response.json()["first_name"] == user.first_name
+        assert response.json()["last_name"] == user.last_name
+        assert response.json()["email"] == user.email
+        assert response.json()["swimmers"][0]["first_name"] == swimmer.first_name
+        assert response.json()["swimmers"][0]["last_name"] == swimmer.last_name
+        assert response.json()["swimmers"][0]["birth_date"] == str(swimmer.birth_date)
+        assert response.json()["swimmers"][0]["level"] == swimmer.level.value
